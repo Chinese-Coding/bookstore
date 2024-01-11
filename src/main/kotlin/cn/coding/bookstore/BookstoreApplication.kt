25b9866a01d45d91d0e42df9901654fd4c37f284
zfq <137599793+Chinese-Coding@users.noreply.github.com>
2024-01-11 09:25:47 +0800

    Initial commit

---
+++ src/main/kotlin/cn/coding/bookstore/BookstoreApplication.kt
@@ -0,0 +1,11 @@
+package cn.coding.bookstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookstoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookstoreApplication>(*args)
+}
---
+++ src/test/kotlin/cn/coding/bookstore/BookstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.coding.bookstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
